57b0b1738d9c812229408d03a17989f401adaeda
dingfubing <1165954612@qq.com>
2022-11-03 11:09:01 +0800

    添加bat命令文件

---
+++ printStr.bat
@@ -0,0 +1,18 @@
+@rem 这里是注释，并且不可见,是@起的作用
+@rem echo off 是关闭回显，不会显示以下的命令，直接输出结果
+@rem @echo off 是代表 'echo off'这一句的命令也不显示了，并且关闭命令回显
+@rem echo %var:~n,k%  n代表字符串开始位置 k代表结束位置  (n,k)
+@rem (n, k) k 可省略，~n不可
+@echo off
+set str=superhero
+@rem superhero
+echo str=%str%
+@rem superh
+echo str:~0,5=%str:~0,5%
+@rem erhero
+echo str:~3=%str:~3%
+@rem ero
+echo str:~-3=%str:~-3%
+@rem superh
+echo str:~0,-3=%str:~0,-3%
+pause
